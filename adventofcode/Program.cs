61077c70860b2b967f2b82a0e5b974095a774bd8
aw94 <awat94@hotmail.com>
2020-12-05 15:42:51 +0100

    update

---
+++ adventofcode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using adventofcode.day5;
+
+namespace adventofcode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("./input.txt");
+            var day5 = new day5part2(input);
+            day5.Solve();
+        }
+    }
+}
